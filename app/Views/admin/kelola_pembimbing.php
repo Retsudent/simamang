5d868c0e6507ba8254c7b7becebc7d727bcc893b
Retsudent <sahrulhafis27@gmail.com>
2025-08-20 09:58:17 +0700

    Update pembimbing management and layout improvements

--- app/Views/admin/kelola_pembimbing.php
+++ app/Views/admin/kelola_pembimbing.php
@@ -34,6 +34,7 @@
                 <td><?= esc($row['username']) ?></td>
                 <td>
                   <a href="<?= base_url('admin/atur-bimbingan-pembimbing/' . $row['id']) ?>" class="btn btn-sm btn-info">Atur Bimbingan</a>
+                  <a href="<?= base_url('admin/hapus-pembimbing/' . $row['id']) ?>" class="btn btn-sm btn-danger" onclick="return confirm('Hapus pembimbing ini? Data yang terkait akan dihapus juga.')">Hapus</a>
                 </td>
               </tr>
             <?php endforeach; else: ?>
